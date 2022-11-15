4d71c0be9a1d3d0bfc4d9ed4ea52f9f601549a7e
mortypk <mortypk@gmail.com>
2022-11-16 00:06:24 +0500

    todo files

---
+++ database/factories/TodoFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\todo>
+ */
+class TodoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(5),
+            'status' => $this->faker->boolean(),
+            'category_id' => category::all()->random()->id,
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'nova',
+            'email' => 'xpk123@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => 'asdfasdfsf',
+        ]);
+        \App\Models\Category::insert(['name' => 'My Todo List', 'created_at' => now()]);
+        \App\Models\Category::insert(['name' => 'My Optional Todo', 'created_at' => now()]);
+        
+        \App\Models\todo::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
